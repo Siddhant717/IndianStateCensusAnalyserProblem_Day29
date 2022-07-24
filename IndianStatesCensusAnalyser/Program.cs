71ad2e035bfd6b69fa8e1dcc8febe9ca8b07561f
Siddhant <santoshshrivastava39@gmail.com>
2022-07-24 17:43:44 +0530

    Load the Indian State Census Information

---
+++ IndianStatesCensusAnalyser/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IndianStatesCensusAnalyser
+{
+    class Program
+    {
+        // CSV File Paths 
+        static string indianStateCensusHeaders = "State,Population,AreaInSqKm,DensityPerSqKm";
+
+        static string indianStateCensusFilePath = @"D:\AllAssignment\IndianStateAnalyser\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\CSV\IndiaStateCensusData.csv";
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian State Census Analyser Problem ");
+
+            IndianCensusAdapter indianCensusAdapter = new IndianCensusAdapter();
+
+            // Loading the Census Data File
+            indianCensusAdapter.LoadCensusData(indianStateCensusFilePath, indianStateCensusHeaders);
+        }
+    }
+}
