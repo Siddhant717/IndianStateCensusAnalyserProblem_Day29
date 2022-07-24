9e396a78ad366c40f03d4c78e0329bd7722ff1f4
Siddhant <santoshshrivastava39@gmail.com>
2022-07-24 17:51:52 +0530

    Check Number of Record Matches

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -1,18 +1,36 @@
 using NUnit.Framework;
+using IndianStatesCensusAnalyser;
+using IndianStatesCensusAnalyser.POCO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using static IndianStatesCensusAnalyser.CensusAnalyser;
 
-namespace TestProject
+namespace TestProject1
 {
     public class Tests
     {
+        static string indianStateCensusHeaders = "State,Population,AreaInSqKm,DensityPerSqKm";
+
+        static string indianStateCensusFilePath = @"D:\AllAssignment\IndianStateAnalyser\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\CSV\IndiaStateCensusData.csv";
+
+        IndianStatesCensusAnalyser.CensusAnalyser censusAnalyser;
+        Dictionary<string, CensusDTO> totalRecord;
+        Dictionary<string, CensusDTO> stateRecord;
         [SetUp]
         public void Setup()
         {
+            censusAnalyser = new IndianStatesCensusAnalyser.CensusAnalyser();
+            totalRecord = new Dictionary<string, CensusDTO>();
+            stateRecord = new Dictionary<string, CensusDTO>();
         }
 
+        //count
+        //TC 1.1 - Check to ensure the Number of Record matches
         [Test]
-        public void Test1()
+        public void GivenIndianCensusDataFile_WhenReaded_ShouldReturnCensusDataCount()
         {
-            Assert.Pass();
+            totalRecord = censusAnalyser.LoadCensusData(CensusAnalyser.Country.INDIA, indianStateCensusFilePath, indianStateCensusHeaders);
+            Assert.AreEqual(5, totalRecord.Count);
         }
     }
 }
